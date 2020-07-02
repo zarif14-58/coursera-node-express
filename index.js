e4bf1525d368f047cd77919e785735b2d191b777
zarif14-58 <a.jevan14nzs@gmail.com>
2020-07-02 20:34:35 +0600

    Assignment 1

--- index.js
+++ index.js
@@ -18,24 +18,13 @@ const dishRouter = require('./routes/dishRouter');
 
 app.use('/dishes', dishRouter);
 
-/*app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
+const promoRouter = require('./routes/promoRouter');
 
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
+app.use('/promotions', promoRouter)
 
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
+const leaderRouter = require('./routes/leaderRouter')
 
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});*/
+app.use('/leaders', leaderRouter)
 
 app.use((req, res, next) => {
   console.log(req.headers);
